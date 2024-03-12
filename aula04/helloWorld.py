949430f7837ff04897f0e12ea17ed906e6df7b6e
luyz-gusta <luizgustavo.sp2020@gmail.com>
2024-03-12 17:00:07 -0300

    Enviando aulas

---
+++ aula04/helloWorld.py
@@ -0,0 +1,29 @@
+# **************************************************************************************
+# Objetivo: Aprender base de Python
+# Autor: Luiz Gustavo
+# Data: 07/03/2024
+# Versão: 1.0
+# **************************************************************************************
+
+print("\n***************** Bem vindo ao Sistema *****************\n")
+
+nome = input("Digite o seu nome: ")
+rm = input("Digite o seu rm: ")
+email = input("Digite o seu email: ")
+faculdade = input("Digite a sua faculdade: ")
+curso = input("Digite o seu curso: ")
+
+print("\n********* Seus dados: *********")
+print(f"Nome: {nome} \nRM: {rm} \nemail: {email}\n" + 
+      f"Faculdade: {faculdade} \ncurso: {curso}\n")
+
+
+valor1 = input("V1: ")
+valor2 = input("V2: ")
+print(float(valor1) + float(valor2))
+
+
+v1 = int(input("V1: "))
+v2 = int(input("V2: "))
+print(v1 + v2)
+
